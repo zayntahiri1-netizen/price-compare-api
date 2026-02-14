3a3fa7a188a3ed4afae2c1c8e5deacf990613ef4
zayntahiri1-netizen <zayntahiri1@gmail.com>
2026-02-14 22:32:05 +0100

    Create stores.js

---
+++ api/stores.js
@@ -0,0 +1,17 @@
+export const stores = {
+  ma: [
+    {name:"Jumia", url:"https://www.jumia.ma/catalog/?q="},
+    {name:"Electroplanet", url:"https://www.electroplanet.ma/search?q="},
+    {name:"Marjane", url:"https://www.marjane.ma/catalogsearch/result/?q="},
+    {name:"Carrefour", url:"https://www.carrefour.ma/catalogsearch/result/?q="},
+    {name:"Microchoix", url:"https://www.microchoix.ma/search?q="}
+  ],
+
+  sa: [
+    {name:"Amazon", url:"https://www.amazon.sa/s?k="},
+    {name:"Noon", url:"https://www.noon.com/saudi-en/search?q="},
+    {name:"Jarir", url:"https://www.jarir.com/catalogsearch/result/?q="},
+    {name:"Extra", url:"https://www.extra.com/en-sa/search?q="},
+    {name:"Carrefour", url:"https://www.carrefourksa.com/search?q="}
+  ]
+};
